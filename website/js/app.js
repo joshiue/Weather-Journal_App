de270af883436a1ec24823f50ad937d4ce38d218
Joshua Taiwo <98131071+joshiue@users.noreply.github.com>
2022-07-30 21:59:49 +0100

    Update app.js

--- website/js/app.js
+++ website/js/app.js
@@ -1,105 +1,124 @@
-const baseURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
-const apiKey = '&appid=0b9d0a826b73f811d3ab2f7639e2f5ec';
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?q=";
+const apiKey = "&appid=0b9d0a826b73f811d3ab2f7639e2f5ec";
 
-document.getElementById('generate').addEventListener('click', performAction);
+document.getElementById("generate").addEventListener("click", performAction);
 
 function performAction(e) {
-    e.preventDefault();
-    const form = document.forms["journalForm"]["feelings"].value;
-    if (form == "") {
-        alert("Please describe your mood today.");
-        return false;
-    } else {
-        const city = document.getElementById('city').value;
-        const journalEntry = document.getElementById('feelings').value;
-        getWeather(baseURL, city, apiKey)
-        .then(data => {
-            postData('/addEntry', { data: data, mood: journalEntry });
-            updateUI();
-        });
-    };
-    clearResults();
-};
- 
-const getWeather = async (baseURL, city, key) => {
-    const response = await fetch(baseURL+city+key);
-    const data = await response.json();
-    try {
-        return {
-            city: data.name,
-            country: data.sys.country,
-            temp: data.main.temp,
-            description: data.weather[0].description,
-            icon: data.weather[0].icon
-        };
-    } catch (error) {
-        console.log("Error", error);
-    };
-};
-
-const postData = async (url = '', data = {}) => {
-    const response = await fetch(url, {
-        method: 'POST',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+  e.preventDefault();
+  const form = document.forms["journalForm"]["feelings"].value;
+  if (form == "") {
+    alert("How you feel today.");
+    return false;
+  } else {
+    const city = document.getElementById("city").value;
+    const journalEntry = document.getElementById("feelings").value;
+    getWeather(baseURL, city, apiKey).then((data) => {
+      postData("/addEntry", { data: data, mood: journalEntry });
+      journal();
     });
-    try {
-        const getData = await response.json();
-        return getData;
-    } catch (error) {
-        console.log("Error:", error);
+  }
+  clearResults();
+}
+
+const getWeather = async (baseURL, city, key) => {
+  const response = await fetch(baseURL + city + key);
+  const data = await response.json();
+  try {
+    return {
+      city: data.name,
+      country: data.sys.country,
+      temp: data.main.temp,
+      description: data.weather[0].description,
+      icon: data.weather[0].icon,
     };
+  } catch (error) {
+    console.log("Error", error);
+  }
 };
 
-// Update UI
-const updateUI = async () => {
-    const request = await fetch('/all');
-    try {
-        const allData = await request.json();
-        for(let i = 0; i < allData.length; i++) {
-            const data = allData[i].data;
-            const mood = allData[i].mood;
-            let d = new Date();
-            const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-            const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-            const cTemp = document.getElementById('celsius');
-            const fTemp = document.getElementById('fahrenheit');
-            const tempSection = document.querySelector('.temp');
-            let degree = document.querySelector('.degree');
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  try {
+    const getData = await response.json();
+    return getData;
+  } catch (error) {
+    console.log("Error:", error);
+  }
+};
 
-            // Formula to convert Kelvin to Celcius
-            let celsius = data.temp - 273.15;
+// Weather Journal log
+const journal = async () => {
+  const request = await fetch("/all");
+  try {
+    const entries = await request.json();
+    for (let i = 0; i < entries.length; i++) {
+      const data = entries[i].data;
+      const mood = entries[i].mood;
+      let d = new Date();
+      const days_of_week = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      const c = document.getElementById("celsius");
+      const f = document.getElementById("fahrenheit");
+      const temp = document.querySelector(".temp");
+      let degree = document.querySelector(".degree");
 
-            // Formula to convert Celcius to Fahrenheit
-            let fahrenheit = (celsius * 1.8) + 32;
+      // Kelvin to Celcius
+      let celsius = data.temp - 273.15;
 
-            document.querySelector('.journal-entry').textContent = `Journal Entry #${i+1}`;
-            document.querySelector('.current-date').textContent = `${days[d.getDay()]} ${d.getDate()} ${months[d.getMonth()]}, ${d.getFullYear()}`;
-            document.querySelector('.name').textContent = `${data.city}, ${data.country}`;
-            document.querySelector('.current-weather').textContent = `${data.description}`;
-            document.querySelector('.weather-icon').src = `http://openweathermap.org/img/w/${data.icon}.png`;
-            document.querySelector('.mood').textContent = `${mood}`;
-            degree.textContent = `${Math.floor(celsius)}`;
-            tempSection.style.visibility = 'visible';
+      // Celcius to Fahrenheit
+      let fahrenheit = celsius * 1.8 + 32;
 
-            cTemp.addEventListener('click', () => {
-                degree.textContent = Math.floor(celsius);
-            });
+      document.querySelector(".journal-entry").innerHTML = `WEATHER STATUS`;
+      document.querySelector(".date").innerHTML = `${d.getFullYear()} ${months[d.getMonth()]} ${d.getDate()} ${days_of_week[d.getDay()]}`;
+      document.querySelector(".icon").src = `http://openweathermap.org/img/w/${data.icon}.png`;
+      document.querySelector(".current-weather").innerHTML = `${data.description}`;
+      document.querySelector(".name").innerHTML = `${data.city}, ${data.country}`;
+      document.querySelector(".mood").innerHTML = `${mood}`;
+      degree.innerHTML = `${Math.floor(celsius)}`;
 
-            fTemp.addEventListener('click', () => {
-                degree.textContent = Math.floor(fahrenheit);
-            });
-        }
-    } catch (error) {
-        console.log("Error", error);
-        alert('City/State not found');
-    };
+      c.addEventListener("click", () => {
+        degree.innerHTML = Math.floor(celsius);
+      });
+
+      f.addEventListener("click", () => {
+        degree.textContent = Math.floor(fahrenheit);
+      });
+    }
+  } catch (error) {
+    console.log("Error", error);
+    alert("City/State not found");
+  }
 };
 
 function clearResults() {
-    const form = document.forms["journalForm"];
-    form.reset();
-};
+  const form = document.forms["journalForm"];
+  form.reset();
+}
